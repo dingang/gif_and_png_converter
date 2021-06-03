68fa9c71505cafeacc69ce5bcee941a7fbadf921
Donghwan Jeon <dh6939@naver.com>
2021-06-04 05:35:34 +0900

    Update compress.py

--- compress.py
+++ compress.py
@@ -2,7 +2,7 @@ import os
 import imageio
 from PIL import Image
 
-img_name = "for_test_to"
+img_name = "test"
 
 path = [f"./{img_name}/{i}" for i in os.listdir("./" + img_name)]
 paths = [ Image.open(i) for i in path]
